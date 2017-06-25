5bebb718913fa4c355f60bf86bec6461ea4d634f
George Okello <George Okello>
2017-06-25 17:20:35 -0500

    Add project files.

---
+++ AngularClient/Scripts/MyApp.js
@@ -0,0 +1,120 @@
+﻿var myApp = angular.module('myApp', ['ngRoute']);
+
+//config routing
+myApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            redirectTo : '/home'
+        })
+        .when('/home', {
+            templateUrl: '/template/home/html',
+            controller: 'homeController'
+        })
+        .when('/authenticated', {
+            templateUrl: '/template/authenticate.html',
+            controller: 'authenticateController'
+        })
+        .when('/authorized', {
+            templateUrl: '/template/authorize.html',
+            controller: 'authorizeController'
+        })
+        .when('/login', {
+            templateUrl: '/template/login.html',
+            controller: 'loginController'
+        })
+        .when('/unauthorized', {
+            templateUrl: '/template/unauthorize.html',
+            controller: 'unauthorizeController'
+        })
+}])
+//global variable for store service base path
+myApp.constant('serviceBasePath', 'http://localhost:60010/');
+//controllers
+myApp.controller('homeController', ['$scope', 'dataService', function ($scope, dataService) {
+    //Fetch
+    $scope.data = '';
+    dataService.GetAnonymousData().then(function (data) {
+        $scope.data = data;
+    })
+}])
+
+myApp.controller('authenticateController', ['$scope', 'dataService', function ($scope, dataService) {
+    //Fetch
+    $scope.data = "";
+    dataService.GetAuthenticateData().then(function ($scope, dataService) {
+        $scope.data = data;
+    })
+}])
+
+myApp.controller('loginController', ['$scope', 'dataService', function ($scope, dataService) {
+    //Fetch
+}])
+
+myApp.controller('unauthorizeController', ['$scope', 'dataService', function ($scope, dataService) {
+    //Fetch
+}])
+//services
+myApp.factory('dataService', ['$http', 'serviceBasePath', function ($http, serviceBasePath) {
+    var fac = {};
+    fac.GetAnonymousData = function () {
+        return $http.get(serviceBasePath + '/api/data/forall').then(function (response) {
+            return response.data;
+        })
+    }
+
+    fac.GetAuthenticateData = function () {
+        return $http.get(serviceBasePath + '/api/data/authenticate').then(function (response) {
+            return response.data;
+        })
+    }
+
+    fac.GetAuthorizeData = function () {
+        return $http.get(serviceBasePath + '/api/data/authorize').then(function (response) {
+            return response.data;
+        })
+    }
+    return fac;
+}])
+
+myApp.factory('userSeervice', function () {
+    var fac = {};
+    fac.CurrentUser = null;
+    fac.SetCurrentUser = function (user) {
+        fac.CurrentUser = user;
+        sessionStorage.user = angular.toJson(user);
+    }
+    fac.GetCurrentUser = function () {
+        fac.CurrentUser = angular.fromJson(sessionStorage.user);
+        return fac.CurrentUser;
+    }
+    return fac;
+})
+
+myApp.factory('accountService', ['$http', '$q', 'serviceBasePath', function ($http, $q, serviceBasePath) {
+    var fac = {};
+    fac.login = function (user) {
+        var obj = { 'username': user.username, 'password': user.password, 'grant-type': 'password' };
+        Object.toparams = function ObjectToParams(obj) {
+            var p = [];
+            for (var key in obj) {
+                p.push(key + '=' + encodeURIComponent(obj[key]));
+            }
+            return p.join('&');
+        }
+
+        var defer = $q.defer();
+        $http({
+            method: 'post',
+            url: serviceBasePath + "/token",
+            data: ObjectToParams(obj),
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+        }).then(function (response) {
+            defer.resolve(response.data);
+            }, function (error) {
+                defer.reject(error.data);
+            })
+
+        return defer.promise;
+    }
+}])
+//http interceptor
---
+++ NewWebApiTokApp/MyAuthorizationServerProvider.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security.OAuth;
+using System.Security.Claims;
+
+namespace NewWebApiTokApp
+{
+    public class MyAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated(); // Validate the client
+        }
+
+        // Validate the user
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            if (context.UserName == "admin" && context.Password == "admin")
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, "admin"));
+                identity.AddClaim(new Claim("username", "admin"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "George Okello"));
+            }
+            else if (context.UserName == "user" && context.Password == "user")
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, "user"));
+                identity.AddClaim(new Claim("username", "user"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "Arthur Okello"));
+                context.Validated(identity);
+            }
+            else
+            {
+                context.SetError("invalid_grant", "Provided username and password is incorrect");
+                return;
+            }
+            
+        }
+    }
+}
